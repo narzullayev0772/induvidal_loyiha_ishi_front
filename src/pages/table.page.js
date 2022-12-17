28d115efe5df0db4b5da826faa71d5a2ccd96c99
narzullayev0772 <alishernarzullayev7777@gmail.com>
2022-12-17 12:06:32 +0500

    Update table.page.js

--- src/pages/table.page.js
+++ src/pages/table.page.js
@@ -5,19 +5,15 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { FullCardContext } from "../contexts/fullcard.context";
-import { Box, Button, IconButton, Tooltip, Typography } from "@mui/material";
-import { MdDelete, MdLoop, MdPersonAdd } from "react-icons/md";
-import AddUser from "../components/addUser";
+import { IconButton, Typography } from "@mui/material";
+import { MdDelete, MdLoop } from "react-icons/md";
 import { useContext, useEffect, useState } from "react";
 import AxiosContext from "../contexts/axios.context";
 import DialogContext from "../contexts/dialog.context";
 import Mydialog from "../components/mydialog";
 import { SnackbarContext } from "../contexts/snackbar.context";
-import Invests from "../components/investitsiyalar";
 
 export default function UsersTable() {
-  const { setFullCard, setFullCardComponent } = useContext(FullCardContext);
   const { openDialog, closeDialog } = useContext(DialogContext);
   const { Request } = useContext(AxiosContext);
   const { handleSnackbarOpen } = useContext(SnackbarContext);
@@ -30,123 +26,95 @@ export default function UsersTable() {
   }, [reload]);
 
   return (
-    <Box>
-      <Box
-        sx={{
-          display: "flex",
-          my: 2,
-        }}
-      >
-        <Button
-          variant="contained"
-          startIcon={<MdPersonAdd />}
-          onClick={() => {
-            setFullCard(true);
-            setFullCardComponent(<AddUser setReload={setReload} />);
-          }}
-        >
-          Add User
-        </Button>
-      </Box>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Users</TableCell>
-              <TableCell align="right">Posts count</TableCell>
-              <TableCell align="right">Role</TableCell>
-              <TableCell align="right">Update</TableCell>
-              <TableCell align="right">Delete</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows?.length > 0 &&
-              rows.map((row, index) => (
-                <TableRow
-                  key={index}
-                  sx={{
-                    "&:last-child td, &:last-child th": { border: 0 },
-                    "&:odd": {
-                      backgroundColor: "#00000010",
-                    },
-                    "&:hover": {
-                      backgroundColor: "#00000010",
-                      transition: "all 0.3s ease",
-                    },
-                  }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="right">
-                    <Typography variant="caption">
-                      {row.posts.length}
-                    </Typography>
-                  </TableCell>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Users</TableCell>
+            <TableCell align="right">Posts count</TableCell>
+            <TableCell align="right">Role</TableCell>
+            <TableCell align="right">Update</TableCell>
+            <TableCell align="right">Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows?.length > 0 &&
+            rows.map((row, index) => (
+              <TableRow
+                key={index}
+                sx={{
+                  "&:last-child td, &:last-child th": { border: 0 },
+                  "&:odd": {
+                    backgroundColor: "#00000010",
+                  },
+                  "&:hover": {
+                    backgroundColor: "#00000010",
+                    transition: "all 0.3s ease",
+                  },
+                }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="caption">{row.posts.length}</Typography>
+                </TableCell>
 
-                  <TableCell align="right">
-                    <Typography
-                      variant="caption"
-                      sx={{
-                        padding: "2px 10px",
-                        border: "1px solid orange",
-                        borderRadius: "15px",
-                      }}
-                    >
-                      {row.role}
-                    </Typography>
-                  </TableCell>
-                  <TableCell align="right">
-                    <IconButton
-                      variant="contained"
-                      color="warning"
-                      size="small"
-                    >
-                      <MdLoop />
-                    </IconButton>
-                  </TableCell>
-                  <TableCell align="right">
-                    <IconButton
-                      variant="contained"
-                      color="error"
-                      size="small"
-                      onClick={() => {
-                        openDialog(
-                          <Mydialog
-                            title={"Rostdan ham o'chirishni istaysizmi?"}
-                            onNo={closeDialog}
-                            onYes={async () => {
-                              try {
-                                await Request(
-                                  `/api/users/${row._id}`,
-                                  "DELETE"
-                                );
-                                setReload((prev) => prev + 1);
-                                closeDialog();
-                                handleSnackbarOpen({
-                                  message: "O'chirildi",
-                                  severity: "success",
-                                });
-                              } catch (error) {
-                                console.log(error);
-                                handleSnackbarOpen({
-                                  message: "Xatolik yuz berdi",
-                                  severity: "error",
-                                });
-                              }
-                            }}
-                          />
-                        );
-                      }}
-                    >
-                      <MdDelete />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
+                <TableCell align="right">
+                  <Typography
+                    variant="caption"
+                    sx={{
+                      padding: "2px 10px",
+                      border: "1px solid orange",
+                      borderRadius: "15px",
+                    }}
+                  >
+                    {row.role}
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton variant="contained" color="warning" size="small">
+                    <MdLoop />
+                  </IconButton>
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton
+                    variant="contained"
+                    color="error"
+                    size="small"
+                    onClick={() => {
+                      openDialog(
+                        <Mydialog
+                          title={"Rostdan ham o'chirishni istaysizmi?"}
+                          onNo={closeDialog}
+                          onYes={async () => {
+                            try {
+                              await Request(`/api/users/${row._id}`, "DELETE");
+                              setReload((prev) => prev + 1);
+                              closeDialog();
+                              handleSnackbarOpen({
+                                message: "O'chirildi",
+                                severity: "success",
+                              });
+                            } catch (error) {
+                              console.log(error);
+                              handleSnackbarOpen({
+                                message: "Xatolik yuz berdi",
+                                severity: "error",
+                              });
+                            }
+                          }}
+                        />
+                      );
+                    }}
+                  >
+                    <MdDelete />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
